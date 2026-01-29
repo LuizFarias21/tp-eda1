f55f9fe1c949b9dccb158aaed559c05b1af5666e
Ricardo-909-Leal <ricardoeguiff@email.com>
2026-01-29 02:44:43 -0300

    criacao e equeleto de todas as funções do produto

---
+++ produto.h
@@ -0,0 +1,28 @@
+/* .h= define tudo o que ocorre no programa */
+#ifndef PRODUTO_H
+#define PRODUTO_H
+
+typedef struct Produto {
+    int codigo;
+    char nome[100];
+    float preco;
+    struct Produto *prox;
+} Produto;
+
+/* CRUD */
+
+Produto* cadastrarProduto(Produto *lista);
+
+void listarProdutos(Produto *lista);
+
+Produto* buscarProduto(Produto *lista, int codigo);
+
+void editarProduto(Produto *lista);
+
+Produto* removerProduto(Produto *lista, int codigo);
+
+/* Menu */
+
+Produto* menuProdutos(Produto *lista);
+
+#endif
