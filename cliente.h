a67ee87d06036af50f75e799b7309c7f9dfb6485
LuizFarias21 <luizclaudiofarias21@gmail.com>
2026-02-03 05:28:29 -0300

    Simplifica estrutura da lista encadeada de cliente

--- cliente.h
+++ cliente.h
@@ -1,17 +1,13 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
-typedef struct {
+typedef struct cliente {
     int cpf[15];
     char nome[100];
     char email[100];
     char telefone[20];
     char data_nascimento[11];
-} Cliente;
-
-typedef struct celulaCliente {
-    Cliente dados;
     struct celula_cliente *prox;
-} CelulaCliente;
+} Cliente;
 
 #endif
